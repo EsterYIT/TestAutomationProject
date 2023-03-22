b778761a4b7b77483dfe9f40c91d87939e089040
esteryitzhak <esyitzhakli1212@gmail.com>
2023-03-22 18:59:51 +0200

    update Sanity Web

--- src/test/java/sanity/ConduitWeb.java
+++ src/test/java/sanity/ConduitWeb.java
@@ -20,50 +20,50 @@ public class ConduitWeb extends CommonOps {
         UIVerifications.verifyTextElement(conduitUserHomePage.getNav_yourFeed(),"Your Feed");
     }
 
-    @Test(description = "Test03 - Verify Follow Button")
+    @Test(description = "Test02 - Verify Follow Button")
     @Description("Verifies that follow button is clicked")
-    public void test03_verifyFollow()
+    public void test02_verifyFollow()
     {
         WebFlows.signIn(getData("userName"),getData("password"));
         WebFlows.follow();
         UIVerifications.verifyFollowOrUnfollowElement(usersToFollow.getBtn_unFollow(), "Unfollow");
     }
 
-    @Test(description = "Test04 - Verify Unfollow Button")
+    @Test(description = "Test03 - Verify Unfollow Button")
     @Description("Verifies that unfollow button is clicked")
-    public void test04_verifyUnFollow()
+    public void test03_verifyUnFollow()
     {
         WebFlows.signIn(getData("userName"),getData("password"));
         WebFlows.Unfollow();
         UIVerifications.verifyFollowOrUnfollowElement(usersToFollow.getBtn_follow(), "Follow");
     }
 
-    @Test(description = "Test05 - Verify Written A Comment")
+    @Test(description = "Test04 - Verify Written A Comment")
     @Description("Verifies comment has been written")
-    public void test05_verifyCommentIsWritten() throws InterruptedException {
+    public void test04_verifyCommentIsWritten() throws InterruptedException {
         WebFlows.signIn(getData("userName"),getData("password"));
         int commentListSize = WebFlows.writeComment("Test");
         UIVerifications.verifyNumberOfComments(postComment.getComments_list(),commentListSize+1);
     }
 
-    @Test(description = "Test06 - Verify Deleted Comment")
+    @Test(description = "Test05 - Verify Deleted Comment")
     @Description("Verifies comment has been deleted")
-    public void test06_verifyCommentDeleted() throws InterruptedException {
+    public void test05_verifyCommentDeleted() throws InterruptedException {
         WebFlows.signIn(getData("userName"),getData("password"));
         int commentListSize = WebFlows.deleteComment();
         UIVerifications.verifyNumberOfComments(postComment.getComments_list(),commentListSize-1);
     }
 
-    @Test(description = "Test07 - Verify Click On An Image")
+    @Test(description = "Test06 - Verify Click On An Image")
     @Description("This test verifies click on image using sikuli tool, and move to the requested page")
-    public void test07_verifyImageWasClicked() throws FindFailed {
+    public void test06_verifyImageWasClicked() throws FindFailed {
         WebFlows.visualElement("PageTwenty");
         UIVerifications.verifyAClickOnVisualElement("20");
     }
 
-    @Test(description = "Test08 - Verify Popular Tags")
+    @Test(description = "Test07 - Verify Popular Tags")
     @Description("This test verifies the default Popular Tags are displayed (using soft assertion)")
-    public void test08_verifyPopularTags()
+    public void test07_verifyPopularTags()
     {
         UIVerifications.visibilityOfElements(conduitMain.getPopular_tags());
     }
